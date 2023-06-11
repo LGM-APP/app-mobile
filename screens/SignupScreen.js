ca3f640a8fa326ba99be575a126f3d62c68c4660
Amine Siari <amine.siari@etu.univ-amu.fr>
2023-06-11 22:37:07 +0200

    modif page connec

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,40 +1,77 @@
-import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { auth_service } from "../services/auth.service";
 
-const SignupScreen = ({ navigation }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const SignupScreen = () => {
+  const navigation = useNavigation();
+  const logo = "../public/logo.png";
 
-  const handleLogin = () => {
-    navigation.navigate('DrawerNavigator');
-  };
+  const [credentials, setCredentials] = useState({
+    "email": "",
+    "password": "",
+  });
 
-  const handleSignup = () => {
-    navigation.navigate('InscriptionScreen');
-  };  
+  const handleChanges = (name, value) => {
+    setCredentials({
+      ...credentials,
+      [name]: value
+    })
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    auth_service.login(credentials)
+      .then(response => {
+        auth_service.save_token(response.data.accessToken);
+        navigation.navigate("HomeScreen");
+      })
+      .catch(err => console.log(err))
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.loginContainer}>
-        <TextInput
-          value={username}
-          onChangeText={(value) => setUsername(value)}
-          placeholder="Pseudo"
-          style={styles.input}
-        />
-        <TextInput
-          value={password}
-          onChangeText={(value) => setPassword(value)}
-          placeholder="Mot de passe"
-          secureTextEntry
-          style={styles.input}
-        />
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Se connecter</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.signupButton} onPress={handleSignup}>
-          <Text style={styles.buttonText}>S'inscrire</Text>
-        </TouchableOpacity>
+      <View style={styles.content}>
+        <View style={styles.textCenter}>
+          <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+            <Image source={{ logo }} style={styles.logo} />
+          </TouchableOpacity>
+          <View style={styles.spaceY}>
+            <Text style={styles.title}>Connectez-vous à votre compte</Text>
+            <Text>
+              Vous n'avez pas encore de compte ?{' '}
+              <Text
+                style={styles.link}
+                onPress={() => navigation.navigate('InscriptionScreen')}
+              >
+                Inscrivez-vous
+              </Text>
+            </Text>
+          </View>
+        </View>
+        <View style={styles.form}>
+          <Text>Email</Text>
+          <TextInput
+            style={styles.input}
+            name="email"
+            onChangeText={value => handleChanges("email", value)}
+            value={credentials.email}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Text>Mot de passe</Text>
+          <TextInput
+            style={styles.input}
+            name="password"
+            onChangeText={value => handleChanges("password", value)}
+            value={credentials.password}
+            secureTextEntry={true}
+          />
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Connexion</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -45,42 +82,52 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    padding: 16,
+  },
+  content: {
+    maxWidth: 360,
+    width: '100%',
+  },
+  textCenter: {
+    alignItems: 'center',
+  },
+  spaceY: {
+    marginTop: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
   },
-  loginContainer: {
-    width: '80%',
-    padding: 20,
-    borderRadius: 10,
-    backgroundColor: '#f2f2f2',
+  link: {
+    color: '#069',
+  },
+  form: {
+    marginTop: 32,
   },
   input: {
     height: 40,
-    marginBottom: 10,
-    paddingHorizontal: 10,
+    borderColor: '#ccc',
     borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
+    marginTop: 8,
+    paddingHorizontal: 8,
+    borderRadius: 8,
   },
   button: {
-    backgroundColor: '#666',
-    borderRadius: 5,
-    paddingVertical: 10,
-    marginBottom: 10,
-  },
-  signupButton: {
-    backgroundColor: '#ccc',
-    borderRadius: 5,
-    paddingVertical: 10,
+    backgroundColor: '#069',
+    marginTop: 16,
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
   },
   buttonText: {
     color: '#fff',
-    textAlign: 'center',
+    fontSize: 16,
     fontWeight: 'bold',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
   },
 });
 
