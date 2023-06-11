4584b3ec2160bcd455aad30dbaf59d634c4c24ba
Amine Siari <amine.siari@etu.univ-amu.fr>
2023-06-11 22:37:54 +0200

    modif page inscrip

--- screens/InscriptionScreen.js
+++ screens/InscriptionScreen.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { auth_service } from '../services/auth.service.js';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const InscriptionScreen = () => {
     const navigation = useNavigation();
@@ -30,35 +30,65 @@ const InscriptionScreen = () => {
     }
 
     return (
-        <View>
-            <Text>Créer un compte</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Créer un compte</Text>
             <TextInput
+                style={styles.input}
                 placeholder="Prénom"
                 onChangeText={value => handleChanges('firstName', value)}
                 value={credentials.firstName}
             />
             <TextInput
+                style={styles.input}
                 placeholder="Nom"
                 onChangeText={value => handleChanges('lastName', value)}
                 value={credentials.lastName}
             />
             <TextInput
+                style={styles.input}
                 placeholder="Email"
                 onChangeText={value => handleChanges('email', value)}
                 value={credentials.email}
             />
             <TextInput
+                style={styles.input}
                 placeholder="Mot de passe"
                 secureTextEntry
                 onChangeText={value => handleChanges('password', value)}
                 value={credentials.password}
             />
-            <Button
-                title="Créer un compte"
-                onPress={handleRegister}
-            />
+            <View style={styles.buttonContainer}>
+                <Button
+                    title="Créer un compte"
+                    onPress={handleRegister}
+                />
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        marginBottom: 15,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+    buttonContainer: {
+        marginTop: 20,
+    }
+});
 
 export default InscriptionScreen;
