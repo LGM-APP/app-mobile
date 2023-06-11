4d1b7ed40ba9771b7c778d26dab63e811c1ab066
Amine Siari <amine.siari@etu.univ-amu.fr>
2023-06-11 22:48:33 +0200

    modif page insc

--- screens/InscriptionScreen.js
+++ screens/InscriptionScreen.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
-import { auth_service } from '../services/auth.service.js';
-import { useNavigation } from '@react-navigation/native';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import {auth_service} from '../services/auth.service';
+import {useNavigation} from '@react-navigation/native';
+import {View, Text, TextInput, TouchableOpacity, StyleSheet, Image} from 'react-native';
 
 const InscriptionScreen = () => {
     const navigation = useNavigation();
+    const logo = 'chemin_vers_votre_logo';
 
     const [credentials, setCredentials] = useState({
         "firstName": "",
@@ -24,44 +25,53 @@ const InscriptionScreen = () => {
         auth_service.register(credentials)
             .then(response => {
                 auth_service.save_token(response.data.accessToken);
-                navigation.navigate("SignupScreen");
+                navigation.navigate('SignupScreen'); // Assurez-vous que vous avez une route nommée 'Login'
             })
             .catch(err => console.log(err))
     }
 
     return (
         <View style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.navigate('DrawerNavigator')}>
+                <Image source={{uri: logo}} style={styles.logo} />
+            </TouchableOpacity>
             <Text style={styles.title}>Créer un compte</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Prénom"
-                onChangeText={value => handleChanges('firstName', value)}
-                value={credentials.firstName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Nom"
-                onChangeText={value => handleChanges('lastName', value)}
-                value={credentials.lastName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                onChangeText={value => handleChanges('email', value)}
-                value={credentials.email}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Mot de passe"
-                secureTextEntry
-                onChangeText={value => handleChanges('password', value)}
-                value={credentials.password}
-            />
-            <View style={styles.buttonContainer}>
-                <Button
-                    title="Créer un compte"
-                    onPress={handleRegister}
+            <Text>Vous avez déjà un compte ?
+                <Text style={styles.link} onPress={() => navigation.navigate('SignupScreen')}>Connectez-vous</Text>
+            </Text>
+            <View style={styles.form}>
+                <Text>Prénom</Text>
+                <TextInput
+                    style={styles.input}
+                    name="firstName"
+                    onChangeText={value => handleChanges('firstName', value)}
+                    value={credentials.firstName}
                 />
+                <Text>Nom</Text>
+                <TextInput
+                    style={styles.input}
+                    name="lastName"
+                    onChangeText={value => handleChanges('lastName', value)}
+                    value={credentials.lastName}
+                />
+                <Text>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    name="email"
+                    onChangeText={value => handleChanges('email', value)}
+                    value={credentials.email}
+                />
+                <Text>Mot de passe</Text>
+                <TextInput
+                    style={styles.input}
+                    name="password"
+                    secureTextEntry
+                    onChangeText={value => handleChanges('password', value)}
+                    value={credentials.password}
+                />
+                <TouchableOpacity style={styles.button} onPress={handleRegister}>
+                    <Text style={styles.buttonText}>Créer un compte</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -73,10 +83,21 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 20,
     },
+    logo: {
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+    },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 20,
+    },
+    link: {
+        color: 'blue',
+    },
+    form: {
+        marginTop: 20,
     },
     input: {
         height: 40,
@@ -86,8 +107,14 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         borderRadius: 5,
     },
-    buttonContainer: {
-        marginTop: 20,
+    button: {
+        backgroundColor: 'cyan',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
     }
 });
 
