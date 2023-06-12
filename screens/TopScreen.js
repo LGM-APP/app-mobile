dbe7424760231759f0d0d2b2b470a5e5ccae549b
Amine Siari <amine.siari@etu.univ-amu.fr>
2023-06-12 23:40:42 +0200

    page top up

--- screens/TopScreen.js
+++ screens/TopScreen.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useEffect, useState} from "react";
+import { user_service } from "../services/user.service";
+import { View, Text, FlatList, StyleSheet, ActivityIndicator } from "react-native";
 
-function TopScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Top</Text>
-    </View>
-  );
-}
+const TopScreen=() => {
+  
+    const [rankList, setrankList] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+  
+          const rankData = await user_service.getUsersRanking();
+          setrankList(rankData);
+        } catch (error) {
+          console.error(
+            "Erreur lors de la récupération des données :",
+            error
+          );
+        }
+      };
+      fetchData();
+    }, []);
+
+    return (
+      <View style={{ width: '50%', backgroundColor: '#ccc', padding: 16 }}>
+        <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>Classement joueurs</Text>
+        {rankList && rankList.length > 0 ? (
+          <View>
+            {rankList.map((rank) => (
+              <View key={rank.id}>
+                <Text>
+                  <Text style={{ fontWeight: 'bold' }}>Nom :</Text>{' '}
+                  {rank.firstName} {rank.lastName}
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: 'bold' }}>Points :</Text>{' '}
+                  {rank.point}
+                </Text>
+              </View>
+            ))}
+          </View>
+        ) : (
+          <Text>Aucun joueur</Text>
+        )}
+      </View>
+    );
+  };
 
 export default TopScreen;
