7f6e7510cf8e1bbe84311a69967fb6775f9552ab
Amine Siari <amine.siari@etu.univ-amu.fr>
2023-06-12 09:03:21 +0200

    modif pagination

--- screens/pagination/Pagination.js
+++ screens/pagination/Pagination.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const MAX_PAGE_NUMBERS = 5; // Nombre maximum de numéros de pages à afficher
@@ -44,65 +44,86 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   }
 
   return (
-    <View>
-      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={handlePreviousPage}
+        disabled={currentPage === 1}
+        style={[
+          styles.pageButton,
+          styles.firstButton,
+          currentPage === 1 && styles.disabledButton,
+        ]}
+      >
+        <Text style={styles.pageButtonText}>Previous</Text>
+      </TouchableOpacity>
+      {getPageNumbers().map((pageNumber, index) => (
         <TouchableOpacity
-          onPress={handlePreviousPage}
-          disabled={currentPage === 1}
-          style={{
-            opacity: currentPage === 1 ? 0.5 : 1,
-            paddingHorizontal: 10,
-            paddingVertical: 5,
-            borderTopLeftRadius: 5,
-            borderBottomLeftRadius: 5,
-            borderWidth: 1,
-            borderColor: '#ccc',
-            backgroundColor: '#fff',
-          }}
+          key={index}
+          onPress={() => onPageChange(pageNumber)}
+          style={[
+            styles.pageButton,
+            pageNumber === currentPage && styles.currentButton,
+          ]}
         >
-          <Text style={{ color: '#000' }}>Previous</Text>
-        </TouchableOpacity>
-        {getPageNumbers().map((pageNumber, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => onPageChange(pageNumber)}
-            style={{
-              paddingHorizontal: 10,
-              paddingVertical: 5,
-              borderWidth: 1,
-              borderColor: pageNumber === currentPage ? '#000' : '#ccc',
-              backgroundColor: pageNumber === currentPage ? '#000' : '#fff',
-            }}
+          <Text
+            style={[
+              styles.pageButtonText,
+              pageNumber === currentPage && styles.currentButtonText,
+            ]}
           >
-            <Text
-              style={{
-                color: pageNumber === currentPage ? '#fff' : '#000',
-                fontWeight: pageNumber === currentPage ? 'bold' : 'normal',
-              }}
-            >
-              {pageNumber}
-            </Text>
-          </TouchableOpacity>
-        ))}
-        <TouchableOpacity
-          onPress={handleNextPage}
-          disabled={currentPage === totalPages}
-          style={{
-            opacity: currentPage === totalPages ? 0.5 : 1,
-            paddingHorizontal: 10,
-            paddingVertical: 5,
-            borderTopRightRadius: 5,
-            borderBottomRightRadius: 5,
-            borderWidth: 1,
-            borderColor: '#ccc',
-            backgroundColor: '#fff',
-          }}
-        >
-          <Text style={{ color: '#000' }}>Next</Text>
+            {pageNumber}
+          </Text>
         </TouchableOpacity>
-      </View>
+      ))}
+      <TouchableOpacity
+        onPress={handleNextPage}
+        disabled={currentPage === totalPages}
+        style={[
+          styles.pageButton,
+          styles.lastButton,
+          currentPage === totalPages && styles.disabledButton,
+        ]}
+      >
+        <Text style={styles.pageButtonText}>Next</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row', 
+    justifyContent: 'center'
+  },
+  pageButton: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: '#fff',
+  },
+  firstButton: {
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+  },
+  lastButton: {
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  pageButtonText: {
+    color: '#000'
+  },
+  currentButton: {
+    borderColor: '#000',
+    backgroundColor: '#000',
+  },
+  currentButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  disabledButton: {
+    opacity: 0.5,
+  },
+});
 
 export default Pagination;
