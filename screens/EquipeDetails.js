7261df206376b2cf5d36a6f679b4de9bef7c5f20
Amine Siari <amine.siari@etu.univ-amu.fr>
2023-06-12 12:43:20 +0200

    modif equip detail 1

--- screens/EquipeDetails.js
+++ screens/EquipeDetails.js
@@ -1,4 +1,3 @@
-// EquipeDetails.js
 import React, {useState, useEffect} from "react";
 import { View, Text, StyleSheet, Image, Dimensions, FlatList } from "react-native";
 import { matchs_service } from "../services/matchs.service";
@@ -27,7 +26,9 @@ const EquipeDetails = ({ route }) => {
       <View style={styles.container}>
         <Text style={styles.title}>Team Details</Text>
         <Text style={styles.subtitle}>{team.name}</Text>
-        <Image style={styles.image} source={{ uri: team.imageUrl }} />
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: team.imageUrl }} />
+        </View>
         <Text style={styles.subtitle}>Matchs</Text>
         <FlatList
           data={matchs}
@@ -53,33 +54,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    backgroundColor: '#F3F4F6',
+    
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
-    marginBottom: 10,
+    color: '#1F2937',
+    marginBottom: 20,
   },
   subtitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#374151',
+    marginBottom: 10,
+  },
+  listItem: {
     fontSize: 18,
     marginBottom: 5,
+    color: '#4B5563',
   },
-  description: {
-    marginBottom: 10,
-  },
-  playersTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginTop: 20,
-    marginBottom: 10,
-  },
-  player: {
-    fontSize: 16,
-    marginBottom: 5,
+  imageContainer: {
+    alignSelf: 'center',
+    width: Dimensions.get('window').width * 0.50, // 50% of screen width
+    height: Dimensions.get('window').width * 0.50, // 50% of screen width
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,  
+    elevation: 5,
+    borderRadius: 10, // Optional, if you want the box corners to be rounded
+    marginBottom: 20,
   },
   image: {
-    width: Dimensions.get('window').height * 0.10, // 10% of screen height
-    height: Dimensions.get('window').height * 0.10, // 10% of screen height
-    marginRight: 10,
+    width: '80%', 
+    height: '80%', 
+    resizeMode: 'contain',
   },
 });
 
